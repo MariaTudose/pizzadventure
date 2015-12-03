f82ffc5f6499d828fe3adcb4b31ece825763ca9f
Maria Tudose <maria.tudose@aalto.fi>
2015-12-03 18:19:22 +0200

    added grunt

---
+++ Gruntfile.js
@@ -0,0 +1,34 @@
+module.exports = function(grunt) {
+
+  // Project configuration.
+  grunt.initConfig({
+    pkg: grunt.file.readJSON('package.json'),
+    uglify: {
+      options: {
+        banner: '/*! <%= pkg.name %> <%= grunt.template.today("yyyy-mm-dd") %> */\n'
+      },
+      build: {
+        src: 'src/<%= pkg.name %>.js',
+        dest: 'build/<%= pkg.name %>.min.js'
+      }
+    },
+    connect: {
+      server: {
+        options: {
+            keepalive: true,
+            port: 8080
+      }
+    }
+  }
+  });
+
+
+  grunt.loadNpmTasks('grunt-contrib-uglify');
+  grunt.loadNpmTasks('grunt-contrib-connect');
+
+  grunt.registerTask('default', ['uglify']);
+  grunt.registerTask('server', [
+      'connect:server'
+  ]);
+
+};
