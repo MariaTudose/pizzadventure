814493c13e05645541d1cd0b64794c58d978a224
Maria Tudose <maria.tudose@aalto.fi>
2015-12-04 18:53:04 +0200

    perusjuttui

--- Scripts/game.js
+++ Scripts/game.js
@@ -1,35 +1,65 @@
 var game = new Phaser.Game(800, 600, Phaser.AUTO, '', {preload: preload, create: create, update: update});
 
+var player;
+var platforms;
+
 function preload() {
 
-    //Load images here
-
-    //Example:
-    //game.load.image('logo', 'phaser.png');
+    game.load.image('land', 'images/land.png');
+    game.load.spritesheet('pizza', 'images/pizza.png', 35, 30);
 
 }
 
 function create() {
 
-    //Add physics
     game.physics.startSystem(Phaser.Physics.ARCADE);
 
+    platforms = game.add.group();
+    platforms.enableBody = true;
 
-    //Add sprites (game objects)
+    var ground = platforms.create(0, game.world.height - 35, 'land');
+    ground.body.immovable = true;
 
-    //Example:
-    //var logo = game.add.sprite(game.world.centerX, game.world.centerY, 'logo');
-    //logo.anchor.setTo(0.5, 0.5);
+    player = game.add.sprite(game.world.width - 35, game.world.height - 70, 'pizza');
+    player.scale.setTo(1, 1);
+    player.enableBody = true;
+    game.physics.arcade.enable(player);
 
+    player.body.gravity.y = 300;
+    player.body.collideWorldBounds = true;
+
+    player.animations.add('left', [0, 1, 2, 3], 10, true);
+    player.animations.add('right', [5, 6, 7, 8], 10, true);
+
+    cursors = game.input.keyboard.createCursorKeys();
 
 }
 
 function update() {
 
-    //Called every frame
 
-    //Collision check:
-    //game.physics.arcade.collide(player, platforms);
+    game.physics.arcade.collide(player, platforms);
+
+
+    player.body.velocity.x = 0;
+
+    if (cursors.left.isDown) {
+        player.body.velocity.x = -350;
+        player.animations.play('left');
+    }
+    else if (cursors.right.isDown) {
+        player.body.velocity.x = 350;
+        player.animations.play('right');
+    }
+    else{
+        player.animations.stop();
+        player.frame = 4;
+    }
+
+    if (cursors.up.isDown && player.body.touching.down) {
+        player.body.velocity.y = -300;
+    }
+
 
     //Check overlap:
     //game.physics.arcade.overlap(player, items, collectItem, null, this);
